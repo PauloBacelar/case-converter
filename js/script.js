f32d15f498b021001a0c4d9f547e53e2cbd5ac9d
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-03-21 09:26:08 -0300

    Created functions changePlaceholder() and getText(). Created some variables too

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 // Variables
 const textArea = document.querySelector("textarea#text-input");
+const buttons = document.querySelectorAll("button.button");
+var placeholder = "Type or paste your content here";
+var text;
 
 // Functions
 function changeTextArea(newText) {
   textArea.value = newText;
 }
+
+function changePlaceholder(newPlaceholderTxt) {
+  textArea.placeholder = newPlaceholderTxt;
+}
+
+function getText() {
+  return textArea.value;
+}
+
+// Event listener
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    text = getText();
+  });
+});
