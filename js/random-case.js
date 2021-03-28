68f2513790cbeb0ab6f715b2d7fbd2e2ab78c921
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-03-28 09:20:45 -0300

    Finished with random-case.js

--- js/random-case.js
+++ js/random-case.js
@@ -3,5 +3,24 @@ const randomCaseButton = document.querySelector("button#random");
 
 // Functions
 function toRandom(txt) {
-    
+  let newText = "";
+  for (let c in txt) {
+    let randomNum = Math.floor(Math.random() * 2);
+
+    if (randomNum === 0) {
+      newText += txt[c].toLowerCase();
+    } else {
+      newText += txt[c].toUpperCase();
+    }
+  }
+
+  return newText;
 }
+
+randomCaseButton.addEventListener("click", () => {
+  if (getText()) {
+    changeTextArea(toRandom(getText()));
+  } else {
+    changePlaceholder(toRandom("Type or paste your content here"));
+  }
+});
