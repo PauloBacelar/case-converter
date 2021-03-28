46d49240d6eb611a07ba54f7139efa132563722e
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-03-28 09:29:38 -0300

    Copy to clipboard button is now working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Variables
 const textArea = document.querySelector("textarea#text-input");
 const buttons = document.querySelectorAll("button.button");
+const copyButton = document.querySelector("button#copy-to-copyboard");
 var placeholder = "Type or paste your content here";
 var text;
 
@@ -22,9 +23,16 @@ function getText() {
   return textArea.value;
 }
 
+function copyText() {
+  textArea.select();
+  document.execCommand("copy");
+}
+
 // Event listener
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     text = getText();
   });
 });
+
+copyButton.addEventListener("click", copyText);
